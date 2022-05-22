a240b96a2a9f35f6bea5011ad7ec3d299d0454c0
Mr@JEY <91868353+PhanNguyenHoangBao-ITITIU20165@users.noreply.github.com>
2022-05-22 22:59:00 +0700

    login react

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import login from "./login";
+import Toggle from "./Toggle";
+import './App.css';
+
+function App() {
+  return (
+      <div>
+          <Toggle />
+      </div>
+  );
+}
+
+export default App;
---
+++ src/Toggle.js
@@ -0,0 +1,21 @@
+import React, {useState} from "react";
+import Login from "./login";
+import Register from "./register";
+import './Toggle.css';
+
+export  default function Toggle(){
+    const [show,setShow] = useState(false);
+
+    return (
+        <div>
+            <div>
+                {
+                    show ? <Register/> : <Login />
+                }
+            </div>
+            <div className="base">
+                <button onClick={() => setShow(!show)}>Click</button>
+            </div>
+        </div>
+    );
+}
